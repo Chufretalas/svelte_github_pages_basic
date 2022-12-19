bca2e83d5d2f695e8d4906e47bb6d604d576a551
Chufretalas <marcobene.contact@gmail.com>
2022-12-19 11:28:17 -0300

    made the site a bit more fun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {base} from "$app/paths"
+</script>
+
+<img src="{base}/caovo.png" alt="a dog that looks like an egg" id="caovo-img" />
+
+<style>
+    @keyframes rotate {
+        0% {
+            transform: rotate3d(1,1,1, 0);
+        }
+        50% {
+            transform: rotate3d(1,1,1, 720deg);
+        }
+        100% {
+            transform: rotate3d(1,1,1, 360deg);
+        }
+    }
+
+    #caovo-img {
+        max-height: 60vh;
+        animation-name: rotate;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        animation-direction: alternate;
+        animation-timing-function: ease-in-out;
+    }
+    
+    #caovo-img:active {
+        animation-name: none;
+        transform: scale(0.5);
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -13,7 +15,7 @@ const config = {
 			assets: "docs"
 		}),
 		paths: {
-			base: "/svelte_github_pages_basic",
+			base: dev ? '' : '/svelte_github_pages_basic',
 		},
 	}
 };
